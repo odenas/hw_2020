e990066a726fa2c3cf79374317ed526e2e727e5f
Olgert Denas <gertidenas@gmail.com>
2020-11-23 18:36:22 -0800

    refactor tie strengths

--- scripts/tie_strengths.py
+++ scripts/tie_strengths.py
@@ -12,6 +12,19 @@ from ghw.weights import Weight
 
 log = logging.getLogger(__name__)
 
+
+def length_first(a, b, w):
+    return len(a)
+
+
+def length_second(a, b, w):
+    return len(b)
+
+
+def length_both(a, b, w):
+    return len(a & b)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('artists', type=str, help='Artist data file')
@@ -32,9 +45,10 @@ if __name__ == '__main__':
     Y = int(os.path.splitext(args.socioMatrix)[0].split("_")[1])
     weights = W.weights_of("films")
 
-    # TODO this is too terse
-    sim_functions = [lambda a, b, w: len(a), lambda a, b, w: len(b), lambda a, b, w: len(a & b)]
-    tstrengths = list(zip(*map(lambda f: D.adj_matrix(Y, "film", actors, weights, similarity_function=f, lag=args.lag),
-                          sim_functions)))
-    # pickle.dump(tstrengths, open(args.output, 'w'))
+    adj_matrices = [
+        D.adj_matrix(Y, "film", actors, weights, similarity_function=length_first, lag=args.lag),
+        D.adj_matrix(Y, "film", actors, weights, similarity_function=length_second, lag=args.lag),
+        D.adj_matrix(Y, "film", actors, weights, similarity_function=length_both, lag=args.lag),
+    ]
+    tstrengths = list(zip(*adj_matrices))
     pklSave(args.output, tstrengths)
