f89a6f80340eabe0f9d19ace10e5015ffb7d5d0a
olgert denas <gertidenas@gmail.com>
2020-11-25 00:48:47 -0800

    reports csv are saved through pandas

--- scripts/report.py
+++ scripts/report.py
@@ -3,6 +3,8 @@
 import logging
 
 import argparse
+import pandas
+
 from ghw import pklLoad
 from ghw.blacklist_data import BlacklistData
 from ghw.affiliations import Affiliations
@@ -12,17 +14,20 @@ from ghw.report import Report
 log = logging.getLogger(__name__)
 
 
-def main(report, sep):
+def row_iter(report):
     receiver_card = set()
-    log.info("dumping ...")
-    print(sep.join(report.header))
     for ((s, y), t) in filter(lambda _t: _t[0][1] == Y, B.trials.items()):
         for r in SM.V.keys():
             if s == r:
                 continue
             _out_row = report.get(s, r, Y, t, len(receiver_card))
-            print(sep.join(_out_row))
+            yield _out_row
             receiver_card |= set([r])
+
+
+def main(report, out_file):
+    log.info("dumping ...")
+    pandas.DataFrame(list(row_iter(report)), columns=report.header).to_csv(out_file, index=False)
 
 
 if __name__ == '__main__':
@@ -30,7 +35,7 @@ if __name__ == '__main__':
     parser.add_argument('network', nargs=3, type=str,
                         help='SocioMatrix, BlockMatrix, TieStrengths in that order.')
     parser.add_argument('blacklist', type=str, help='Blacklist data file')
-    parser.add_argument('-s', '--sep', type=str, nargs='?', default=', ', help='Record separator')
+    parser.add_argument('output', type=str, help='Output file')
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG)
 
@@ -46,4 +51,4 @@ if __name__ == '__main__':
     A = Affiliations()
     B = BlacklistData(args.blacklist)
 
-    main(Report(Y, A, B, SM, BM, TS, BM.distances, SM.relations), args.sep)
+    main(Report(Y, A, B, SM, BM, TS, BM.distances, SM.relations), args.output)
