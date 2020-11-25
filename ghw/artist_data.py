d4e41e3f6a6d465728687ba213d91e5521759fe1
olgert denas <gertidenas@gmail.com>
2020-11-24 23:33:53 -0800

    remove old functions

--- ghw/artist_data.py
+++ ghw/artist_data.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 import csv
 import logging
-from operator import attrgetter, concat
+from operator import concat
 from functools import reduce
 
 import numpy as np
@@ -87,6 +87,7 @@ def selector_prod_house(ainfol):
     ainfo_vals = [a.prod_house for a in ainfol]
     return set(ainfo_vals)
 
+
 def selector_film(ainfol):
     ainfo_vals = [a.film for a in ainfol]
     return set(ainfo_vals)
@@ -103,6 +104,7 @@ selector_functions = {
     'prod_house': selector_prod_house, 'film': selector_film,
     'year': selector_year
 }
+
 
 class ArtistInfoData(object):
     """loads and maintains a collection of
@@ -156,84 +158,3 @@ class ArtistInfoData(object):
                 op2 = selector(data[a2])
                 matrix[i, j] = similarity_function(op1, op2, weights)
         return V, matrix
-
-
-    def _adj_matrix(self, year, attr, actors, weights,
-                   similarity_function=similarity_function, lag=1000):
-        """compute the adjacency matrix of a relation on
-        the given actors.
-
-        The method will remove all artists that had not have a
-        career until now.
-
-        :param int year: the features of actors after or on
-            this year will be filtered out
-        :param string attr: defines the relation. this is
-            an attribute of :py:class:`ArtistInfoRow`
-        :param list actors: the matrix will be based on these
-            actors
-        :param callable similarity_function: function defined
-            on two sets or integers giving the similarity.
-        :param dict weights: has the weight for each value of the relation domain
-        :param int lag: nr. of years back for which to
-            consider records. So records will be in the
-            period (year-lag, year) both excluded
-        :return: a tuple (list of actor_ids, numpy matrix)
-        """
-
-        # a projection of self.data, s.t.
-        # actors have had a career before this year
-        log.info("filtering out career-less actors ...")
-        data = {}
-        for act in actors:
-            career = list(filter(lambda r: (year - lag) < r.year < year, self.data.get(act, [])))
-            if career:
-                data[act] = career
-            else:
-                log.debug("dropping actor %s", act)
-        log.info("indexing (%d) actors ..." % len(data))
-        # actor indexes in the matrix
-        V = dict(map(lambda i_a: (i_a[1], i_a[0]),
-                 enumerate(filter(lambda a: a in data, actors))))
-
-        matrix = np.zeros((len(V), len(V)), dtype=np.float32)
-        for a1 in tqdm(V.keys()):
-            op1 = self.__selector(data[a1], attr)
-            i = V[a1]
-            for a2 in V.keys():
-                j = V[a2]
-                op2 = self.__selector(data[a2], attr)
-                matrix[i, j] = similarity_function(op1, op2, weights)
-        return V, matrix
-
-    def __selector(self, ainfol, sname):
-        """selects attributes of a set of artists and packs
-        them into an int or a set. this value can be feeded as
-        an operant to :py:func:`similarity_function`. Note that
-        this is a private method and should be called by
-        adj_matrix
-
-        :param iterable ainfol: list of artists (instances of
-            :py:class:`ArtistInfoRow`
-        :param string sname: attribute of
-            :py:class:`ArtistInfoRow` to select
-        :returns: set or int
-        """
-
-        if not ainfol:
-            raise ValueError("empty list of artists")
-
-        ainfo_vals = map(attrgetter(sname), ainfol)
-        assert ainfo_vals, "ainfol was not empty, but this is"
-
-        if sname in ("role", "genre"):
-            return set(reduce(concat, ainfo_vals, ()))
-        elif sname in ("prod_house", "film"):
-            return set(ainfo_vals)
-        elif sname in ("year",):
-            return len(set(ainfo_vals))
-        elif sname in ("nominated", "champ"):
-            return len(list(filter(lambda p: p, ainfo_vals)))
-        else:
-            assert not (sname in self.sim_attributes)
-            raise ValueError("cannot handle %s (%r)" % (sname, type(sname)))
