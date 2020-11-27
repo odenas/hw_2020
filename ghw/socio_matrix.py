acee7aabe4941d1f498512528cb62538bbe3c66c
olgert denas <gertidenas@gmail.com>
2020-11-26 16:02:43 -0800

    integrate db to socio matrices

--- ghw/socio_matrix.py
+++ ghw/socio_matrix.py
@@ -1,51 +1,96 @@
 
-from collections import OrderedDict
+from dataclasses import dataclass
 import logging
+from functools import reduce
+from operator import concat
+import sqlite3
 
-from .matrix import Matrix
-from .artist_data import selector_functions
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
 
 log = logging.getLogger(__name__)
 
 
-class SocioMatrix(Matrix):
-    """matrix defined over the similarity (jaccard) measure. maintains
+@dataclass(eq=False)
+class SocioMatrix:
+    relation: str
+    year: int
+    matrix: np.array
+    artists: dict
+    lag: int = 1000
 
-    M an ordered dict of the type (relation_name) ---> matrix
-    V dict of the type (artist) --> index
+    @classmethod
+    def sim_functions(cls, relation):
+        def sim_set(s1, s2):
+            if s1:
+                return len(s1 & s2) / len(s1)
+            return 0.0
 
-    to initialize needs:
+        def sim_scalar(i, j):
+            return 1 - np.abs(i - j) / max(i, j)
 
-    data :py:class:`giacomo.hollywood.bmat.ArtistInfoData`
-    year int
-    actors iterable
-    relations a dictionary of the type (relation_name) --> attribute_name
-    combined bool
-    kwd other options passed by name to ArtistInfoData.adj_matrix
-    """
+        if relation in {'champ', 'nominated', 'year'}:
+            return sim_scalar
+        elif relation in {'role', 'genre', 'house', 'film'}:
+            return sim_set
+        raise ValueError(f"unknown relation {relation}")
 
-    # TODO: combined should be part of the relations argument
-    def __init__(self, data, year, actors, relations, combined=True, **kwd):
-        self.relations = relations
-        self.M = OrderedDict()
-        self.V = None
-        log.info("sociomatrix (%d actors) over relations:" % len(actors))
-        for relname, relval in relations.items():
-            v, m = data.adj_matrix(year, relval, actors, selector_functions[relval], **kwd)
-            self.M[relname] = m
-            log.info("\t%s (%d actors)" % (relname, len(v)))
-        if not self.V:
-            self.V = v
-        if set(v.keys()) < set(self.V.keys()):
-            self.V = v
+    @classmethod
+    def selector_functions(cls, relation):
+        return {
+            'champ': len,
+            'nominated': sum,
+            'role': lambda tl: set(reduce(concat, tl, ())),
+            'genre': lambda tl: set(reduce(concat, tl, ())),
+            'house': set,
+            'film': set,
+            'year': lambda tl: len(set(tl))}[relation]
 
-        if combined:
-            N = float(len(self.M))
-            self.M["combined"] = sum(self.M.values()) / N
+    @classmethod
+    def _get_df(cls, dbpath, relation, year):
+        def unpack_dash_val(val):
+            try:
+                return tuple(map(int, val.split('-')))
+            except ValueError:
+                return ()
 
-    def get(self, s, r, **kwd):
-        """calls :py:func:`Matrix._get` on all sociomatrices"""
+        query = f"""select
+        artist, {relation} as relation
+        from perf join title on title.id = perf.title
+        where year > {year - cls.lag} and year < {year}
+        """
 
-        mlist = [self.M['combined']] + list(map(lambda rn: self.M[rn], self.relations))
-        return self._get(s, r, mlist, self.V, **kwd)
+        con = sqlite3.connect(dbpath)
+        if relation in ('role', 'genre'):
+            df = (pd.read_sql_query(query, con)
+                  .assign(relation=lambda x: x.relation.apply(unpack_dash_val)))
+        else:
+            df = pd.read_sql_query(query, con)
+        return df
+
+    @classmethod
+    def from_db(cls, dbpath, relation, year):
+        df = cls._get_df(dbpath, relation, year)
+        log.info("%d rows", df.shape[0])
+
+        sel_f = cls.selector_functions(relation)
+        ddf = (df.groupby('artist')
+               .aggregate(sel_f)
+               .reset_index().reset_index())
+        log.info("%d actors", ddf.shape[0])
+        v, m = cls.adj_matrix(ddf, relation)
+        return cls(relation, year, m, v)
+
+    @classmethod
+    def adj_matrix(cls, ddf, relation):
+        sim_f = cls.sim_functions(relation)
+
+        m = np.zeros((ddf.shape[0], ddf.shape[0]))
+
+        cprod = ddf.assign(key=1).merge(ddf.assign(key=1), on='key').drop('key', 1)
+        for t in tqdm(cprod.itertuples()):
+            m[t.index_x, t.index_y] = sim_f(t.relation_x, t.relation_y)
+        v = dict(t for _, t in ddf[['artist', 'index']].iterrows())
+        return v, m
--- scripts/socio_matrix.py
+++ scripts/socio_matrix.py
@@ -1,102 +1,27 @@
 """
-Build and dump one or more socio matrices
+Build and dump a socio matrix
 """
 
 import logging
 import os
 import sys
-from collections import OrderedDict
 import argparse
-import sqlite3
-from functools import reduce
-from operator import concat
 
-from tqdm import tqdm
-import numpy as np
-import pandas as pd
-
-from ghw import pklSave, rel_names
-from ghw.matrix import Matrix
-# from ghw.artist_data import ArtistInfoData
-# from ghw.socio_matrix import SocioMatrix
-
+from ghw import pklSave
+from ghw.socio_matrix import SocioMatrix
 
 log = logging.getLogger(__name__)
 
 
-class SocioMatrix1(Matrix):
-    def __init__(self, db_path, year, relations, combined=True, **kwd):
-        self.db_path = db_path
-        self.relations = relations
-        self.M = OrderedDict()
-        self.V = None
-        for relname, relval in relations.items():
-            v, m = self.genre_adj_matrix(year, 1000)
-            self.M[relname] = m
-            log.info("\t%s (%d actors)" % (relname, len(v)))
-        if not self.V:
-            self.V = v
-        if set(v.keys()) < set(self.V.keys()):
-            self.V = v
-
-        if combined:
-            N = float(len(self.M))
-            self.M["combined"] = sum(self.M.values()) / N
-
-    def unpack_dash_val(self, val):
-        try:
-            return tuple(map(int, val.split('-')))
-        except ValueError:
-            return ()
-
-    def genre_adj_matrix(self, year, lag):
-        con = sqlite3.connect(self.db_path)
-        with con:
-            query = f"""select
-            artist.castid, perf.genre
-            from perf join artist on perf.artist = artist.castid
-                join title on title.id = perf.title
-            where year > {year - lag} and year < {year}
-            """
-            log.info(query)
-            df = (pd.read_sql_query(query, con)
-                  .assign(genre=lambda x: x.genre.apply(self.unpack_dash_val)))
-            log.info("%d rows", df.shape[0])
-
-        ddf = (df.groupby('castid')
-               .aggregate(lambda tl: set(reduce(concat, tl, ())))
-               .reset_index().reset_index())
-        log.info("%d actors", ddf.shape[0])
-
-        def sim_role(s1, s2):
-            if s1:
-                return len(s1 & s2) / len(s1)
-            return 0
-
-        m = np.zeros((ddf.shape[0], ddf.shape[0]))
-        cprod = ddf.assign(key=1).merge(ddf.assign(key=1), on='key').drop('key', 1)
-        for t in tqdm(cprod.itertuples()):
-            m[t.index_x, t.index_y] = sim_role(t.genre_x, t.genre_y)
-        v = dict(t for _, t in ddf[['castid', 'index']].iterrows())
-        return v, m
-
-    def get(self, s, r, **kwd):
-        """calls :py:func:`Matrix._get` on all sociomatrices"""
-
-        mlist = [self.M['combined']] + list(map(lambda rn: self.M[rn], self.relations))
-        return self._get(s, r, mlist, self.V, **kwd)
-
-
 def main(artist_data, year, relation, output):
-    log.info("%s - %s" % (Y, R))
-    matrix = SocioMatrix1(artist_data, int(year), 
-                         OrderedDict([(relation, rel_names[R])]), lag=args.lag)
+    log.info("%s - %s" % (year, relation))
+    matrix = SocioMatrix.from_db(artist_data, relation, int(year))
     pklSave(output, matrix)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('artists', type=str, help='Artist data file')
+    parser.add_argument('adata', type=str, help='Artist database')
     parser.add_argument('output', type=str,
                         help='Output file (of the type sm_year_relation.pkl')
     parser.add_argument('--lag', type=int, choices=(5, 1000), default=1000,
@@ -109,4 +34,4 @@ if __name__ == '__main__':
         R = R.split(".")[0]
 
     # sys.exit(main(ArtistInfoData(args.artists), Y, R, args.output))
-    sys.exit(main(args.artists, Y, R, args.output))
+    sys.exit(main(args.adata, Y, R, args.output))
