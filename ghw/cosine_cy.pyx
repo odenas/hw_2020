cf06a5f476a9cef807475d9165e7e4aa8bb79b9c
Olgert Denas <gertidenas@gmail.com>
2020-12-03 00:14:51 -0800

    unroll and add types

--- ghw/cosine_cy.pyx
+++ ghw/cosine_cy.pyx
@@ -1,6 +1,21 @@
 import numpy as np
+cimport cython
 
-def cosine_metric(i, j, m):
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+cdef dot(float[:] a, float[:] b, float correct):
+    cdef Py_ssize_t n = a.shape[0]
+    cdef Py_ssize_t k
+    cdef float dot_row = 0
+    for k in range(n):
+        dot_row += a[k] * b[k]
+    return dot_row - correct
+
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+cdef cosine_metric(Py_ssize_t i, Py_ssize_t j, float[:, :] m):
     """cosine distance
 
     compute distance on the given sociomatrix and artist pair
@@ -10,27 +25,37 @@ def cosine_metric(i, j, m):
     :returns: the distance
     """
 
-    def stabilize(v):
-        if v >= 0:
-            return v
-        if v < -0.000001:
-            raise ValueError("suspicious value: %10f" % v)
+    cdef float epsilon = 1e-4
 
-    dot_row = np.dot(m[i, :], m[j, :]) - (m[i, i]*m[j, i]) - (m[i, j]*m[j, j])
-    dot_col = np.dot(m[:, i],  m[:, j]) - (m[i, i]*m[i, j]) - (m[j, i]*m[j, j])
+    cdef float mii = m[i, i]
+    cdef float mij = m[i, j]
+    cdef float mji = m[j, i]
+    cdef float mjj = m[j, j]
 
-    norm_row_i = np.dot(m[i, :],  m[i, :]) - (m[i, i]*m[i, i]) - (m[i, j]*m[i, j])
-    norm_col_i = np.dot(m[:, i],  m[:, i]) - (m[i, i]*m[i, i]) - (m[j, i]*m[j, i])
-    norm_i = stabilize(norm_row_i + norm_col_i)
+    cdef float[:] mi_ = m[i, :]
+    cdef float[:] m_i = m[:, i]
 
-    norm_row_j = np.dot(m[j, :],  m[j, :]) - (m[j, i]*m[j, i]) - (m[j, j]*m[j, j])
-    norm_col_j = np.dot(m[:, j],  m[:, j]) - (m[i, j]*m[i, j]) - (m[j, j]*m[j, j])
-    norm_j = stabilize(norm_row_j + norm_col_j)
+    cdef float[:] mj_ = m[j, :]
+    cdef float[:] m_j = m[:, j]
+
+    cdef float dot_row = dot(mi_, mj_, (mii*mji) + (mij*mjj))
+    cdef float dot_col = dot(m_i, m_j, (mii*mij) + (mji*mjj))
+
+    cdef float norm_row_i = dot(mi_,  mi_, (mii*mii) + (mij*mij))
+    cdef float norm_col_i = dot(m_i,  m_i, (mii*mii) + (mji*mji))
+    cdef float norm_i = norm_row_i + norm_col_i
+    if norm_i < -epsilon:
+        raise ValueError("suspicious value: %10f" % norm_i)
+
+    cdef float norm_row_j = dot(mj_,  m_j, (mji*mji) + (mjj*mjj))
+    cdef float norm_col_j = dot(m_j,  m_j, (mij*mij) + (mjj*mjj))
+    cdef float norm_j = norm_row_j + norm_col_j
+    if norm_j < -epsilon:
+        raise ValueError("suspicious value: %10f" % norm_j)
 
     if norm_i and norm_j:
         result = (dot_row + dot_col) / (np.sqrt(norm_i) * np.sqrt(norm_j))
-        if abs(result) > 1.001:
+        if result > 1 + epsilon:
             raise ValueError("bad cosine value(%f)! ..." % result)
         return result
-    else:
-        return np.nan
+    return 0.0
